b86a8617a884fa3b5600a0dcbeef7f1f4d20b0f4
ChristelRav <ravmihary@gmail.com>
2025-03-22 07:05:07 +0300

    START

--- models/md_marque.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const MarqueSchema = new mongoose.Schema({
-    nommarque: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Marque', MarqueSchema,'marque');
