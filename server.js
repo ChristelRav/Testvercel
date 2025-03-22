7271a4b3a6382fe0148bd4199a7c07d05ff2348a
ChristelRav <ravmihary@gmail.com>
2025-03-22 09:15:51 +0300

    TEST4 obsolète oe le farany teo

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const app = express();
 app.use(express.json());
 
 // Tester la connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("✅ Connexion réussie à MongoDB Atlas !"))
-.catch(err => console.log("❌ Erreur de connexion :", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connexion réussie à MongoDB Atlas !");
+  })
+  .catch(err => {
+    console.error("❌ Erreur de connexion à MongoDB :", err);
+    process.exit(1); // Arrêter le serveur en cas d'échec
+  });
+
 
 // Importer les routes
 const testRoutes = require("./routes/testroute");
